14ecc625afb586c1c34d68df32bf6924050a50cd
said.ait <130727119+SaidAit01@users.noreply.github.com>
2025-07-27 00:17:58 +0000

    add first challenge

---
+++ love_Calculator.js
@@ -0,0 +1,13 @@
+
+prompt ('Enter the first name : ')
+prompt ('Enter the second name : ')
+var n = Math.random (); 
+n = n*100; 
+n = Math.floor(n)+1; 
+if (n>51) { 
+    alert ('the love percentage between you two is : '+n+ '%'+ ' congrats your love is real !')
+}
+else { 
+alert ('the love percentage between you two is : '+n+ '%')
+} 
+
